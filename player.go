e45ab5fb74e7b46ed03aac8810cd503de811b3e0
Andreas Leeb <andreas.b.leeb@gmail.com>
2019-02-04 19:14:45 +0100

    Provide json for type Player

--- player.go
+++ player.go
@@ -3,6 +3,6 @@ package vbcore
 // Player defines a player with less properties than User. It's used to show up
 // in the frontend to other users
 type Player struct {
-	UserID   string
-	Username string
+	UserID   string `json:"userID"`
+	Username string `json:"username"`
 }
