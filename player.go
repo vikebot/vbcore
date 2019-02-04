c23f5aed9c53ce20b40054d8c48db048d90f392b
Andreas Leeb <andreas.b.leeb@gmail.com>
2019-02-04 18:47:56 +0100

    Add player type to display in frontend

---
+++ player.go
@@ -0,0 +1,8 @@
+package vbcore
+
+// Player defines a player with less properties than User. It's used to show up
+// in the frontend to other users
+type Player struct {
+	UserID   string
+	Username string
+}
