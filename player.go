057f958c77ecd70cece9ff521071f00c1e8d900f
Andreas Leeb <andreas.b.leeb@gmail.com>
2019-02-05 09:55:30 +0100

    Add playerInfo type

--- player.go
+++ player.go
@@ -6,3 +6,11 @@ type Player struct {
 	UserID   string `json:"userID"`
 	Username string `json:"username"`
 }
+
+// PlayerInfo is a Player with more information
+type PlayerInfo struct {
+	UserID     string `json:"userID"`
+	Username   string `json:"username"`
+	Picture    string `json:"picture"`
+	Permission string `json:"permission"`
+}
